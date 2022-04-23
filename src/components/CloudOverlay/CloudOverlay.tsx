3d48ac9bc551ae2717cd9d449842b6eec8c46a3d
Dan Forder <danfrdr@gmail.com>
2022-04-23 14:38:15 +0100

    added cloud overlay

--- src/components/CloudOverlay/CloudOverlay.tsx
+++ src/components/CloudOverlay/CloudOverlay.tsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import cloud from "../../assets/cloud.png";
+import useInstantiateAtInterval from "../../hooks/useInstantiateAtInterval";
+import { getRandomIntegerInRange } from "../../utils/commonFunctions";
 import "./CloudOverlay.scss";
 
+// TODO: remove clouds from start of array when they reach a length threshold
 const CloudOverlay = () => {
-  const createCloud = () => {
-    const cloudStyles = {};
+  const [clouds, setClouds] = useState<JSX.Element[]>([]);
+
+  const addCloud = () => {
+    // TODO: generate uid for clouds instead of index
+    const newCloud = createCloud(clouds.length);
+    setClouds((prev) => [...prev, newCloud]);
+  };
+
+  useInstantiateAtInterval(30, addCloud);
+  // useInstantiateAtInterval(3, addCloud);
+
+  const generateRandomCloudStyles = () => {
+    const [translate, rotate, width] = [
+      getRandomIntegerInRange(0, 250),
+      getRandomIntegerInRange(-12.5, 12.5),
+      `${getRandomIntegerInRange(5, 12)}%`,
+    ];
+    const transform = `translate(0,${translate}%) rotate(${rotate}deg)`;
+
+    return {
+      transform,
+      width,
+    };
+  };
+
+  const createCloud = (key: number) => {
+    const style = generateRandomCloudStyles();
     return (
       <img
         className="cloud-overlay__cloud"
-        style={cloudStyles}
+        style={style}
         src={cloud}
         alt="cloud"
+        key={key}
       />
     );
   };
 
-  return <div className="cloud-overlay">{createCloud()}</div>;
+  return <div className="cloud-overlay">{clouds}</div>;
 };
 
 export default CloudOverlay;
