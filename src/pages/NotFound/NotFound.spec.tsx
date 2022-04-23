68dca7e64b67e7425b76708b3e10dfa91f446e22
Dan Forder <danfrdr@gmail.com>
2022-04-23 14:53:11 +0100

    Add tests

--- src/pages/NotFound/NotFound.spec.tsx
+++ src/pages/NotFound/NotFound.spec.tsx
@@ -1,7 +1,7 @@
 import { customRender } from "../../utils/testUtils";
 import NotFound from "./NotFound";
 
-export default describe("NotFoundView", () => {
+export default describe("NotFound Page", () => {
   it("Renders", () => {
     const { container } = customRender(<NotFound />);
     expect(container).toMatchSnapshot();
